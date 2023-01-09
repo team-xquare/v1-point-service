edde29b7e387e49304b9503fbb0c3dc1db3a800b
lyutvs <lyutvs@gmail.com>
2023-01-09 20:25:12 +0900

    📑 :: PointStatusEntity

---
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/entity/PointStatusEntity.kt
@@ -0,0 +1,42 @@
+package com.xquare.v1servicepoint.point.entity
+
+import com.github.f4b6a3.uuid.UuidCreator
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_point_status")
+class PointStatusEntity(
+
+    @Id
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    val userId: UUID = UuidCreator.getTimeOrderedEpoch(),
+
+    goodPoint: Int,
+
+    badPoint: Int,
+
+    penaltyLevel: Int,
+
+    isPenaltyRequired: Boolean
+) {
+
+    @field:Column(columnDefinition = "INT", nullable = false)
+    var goodPoint = goodPoint
+        protected set
+
+    @field:Column(columnDefinition = "INT", nullable = false)
+    var badPoint = badPoint
+        protected set
+
+    @field:Column(columnDefinition = "INT", nullable = false)
+    var penaltyLevel = penaltyLevel
+        protected set
+
+    @field:Column(columnDefinition = "TINYINT(1)", nullable = false)
+    var isPenaltyRequired = isPenaltyRequired
+        protected set
+}

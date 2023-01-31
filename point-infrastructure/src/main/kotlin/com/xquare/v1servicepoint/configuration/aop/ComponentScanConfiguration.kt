4eb2807f849cb253b2c643cb5f958b3898c8bf4e
lyutvs <lyutvs@gmail.com>
2023-01-31 11:12:28 +0900

    📑 :: ComponentScanConfiguration

---
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/configuration/aop/ComponentScanConfiguration.kt
@@ -0,0 +1,24 @@
+package com.xquare.v1servicepoint.configuration.aop
+
+import com.xquare.v1servicepoint.BASE_PACKAGE
+import com.xquare.v1servicepoint.annotation.SagaStep
+import com.xquare.v1servicepoint.annotation.UseCase
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.ComponentScan.Filter
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
+
+@Configuration
+@ComponentScan(
+    basePackages = [BASE_PACKAGE],
+    includeFilters = [
+        Filter(
+            type = FilterType.ANNOTATION,
+            classes = [
+                UseCase::class,
+                SagaStep::class
+            ]
+        )
+    ]
+)
+class ComponentScanConfiguration

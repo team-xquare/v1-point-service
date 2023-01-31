289c3cb03f81cc6e9911b31028dab850d34d8ba6
lyutvs <lyutvs@gmail.com>
2023-01-31 11:09:39 +0900

    📑 :: Spi

---
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/annotation/Spi.kt
@@ -0,0 +1,8 @@
+package com.xquare.v1servicepoint.annotation
+
+import java.lang.annotation.Inherited
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Inherited
+annotation class Spi

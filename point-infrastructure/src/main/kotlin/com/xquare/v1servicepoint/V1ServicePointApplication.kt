fb66bc31141e7a09c4d7e84b77e372ac6abbe72f
lyutvs <lyutvs@gmail.com>
2023-01-31 11:14:17 +0900

    ⚡️ :: BasePackage 경로 지정

--- point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/V1ServicePointApplication.kt
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/V1ServicePointApplication.kt
@@ -3,6 +3,8 @@ package com.xquare.v1servicepoint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+internal const val BASE_PACKAGE = "com.xquare.v1servicepoint"
+
 @SpringBootApplication
 class V1ServicePointApplication
 

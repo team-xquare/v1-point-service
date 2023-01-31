b86b22d2513656592efa4b39958447a66025b2ea
lyutvs <lyutvs@gmail.com>
2023-01-31 11:10:10 +0900

    📑 :: UserNotFoundException

---
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/exception/UserNotFoundException.kt
@@ -0,0 +1,11 @@
+package com.xquare.v1servicepoint.point.exception
+
+import com.xquare.v1servicepoint.exception.BaseException
+
+class UserNotFoundException(
+    message: String
+) : BaseException(message, 404) {
+    companion object {
+        const val USER_ID_NOT_FOUND = "User ID is not found even if you are select for it"
+    }
+}

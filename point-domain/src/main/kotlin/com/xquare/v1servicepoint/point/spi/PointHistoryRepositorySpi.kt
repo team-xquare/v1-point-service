e11fcadb5328f3284e96c6752669d176d84f544a
lyutvs <lyutvs@gmail.com>
2023-02-07 14:29:38 +0900

    ⚡️ :: PointHistoryRepository

--- point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/spi/PointHistoryRepositorySpi.kt
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/spi/PointHistoryRepositorySpi.kt
@@ -1,6 +1,12 @@
 package com.xquare.v1servicepoint.point.spi
 
 import com.xquare.v1servicepoint.annotation.Spi
+import com.xquare.v1servicepoint.point.Point
+import java.util.UUID
 
 @Spi
-interface PointHistoryRepositorySpi
+interface PointHistoryRepositorySpi {
+    suspend fun saveUserPoint(userId: UUID, pointId: UUID)
+
+    suspend fun findByPointId(pointId: UUID): Point?
+}

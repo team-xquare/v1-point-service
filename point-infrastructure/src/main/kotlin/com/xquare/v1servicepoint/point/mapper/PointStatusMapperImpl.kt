61ed9fdb2e56a6c071a8c352138a83b99243222e
lyutvs <lyutvs@gmail.com>
2023-02-07 14:30:35 +0900

    ⚡️ :: PointStatusMapperImpl

--- point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/mapper/PointStatusMapperImpl.kt
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/mapper/PointStatusMapperImpl.kt
@@ -2,7 +2,9 @@ package com.xquare.v1servicepoint.point.mapper
 
 import com.xquare.v1servicepoint.point.PointStatus
 import com.xquare.v1servicepoint.point.entity.PointStatusEntity
+import org.springframework.stereotype.Component
 
+@Component
 class PointStatusMapperImpl : PointStatusMapper {
 
     override fun pointStatusEntityToDomain(pointStatusEntity: PointStatusEntity): PointStatus {

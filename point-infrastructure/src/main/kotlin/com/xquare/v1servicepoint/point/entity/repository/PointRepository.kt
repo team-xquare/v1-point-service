985c1ddd293b2c5f09104ed45c18de56190d7919
lyutvs <lyutvs@gmail.com>
2023-02-07 14:30:03 +0900

    ⚡️ :: PointRepository

--- point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/entity/repository/PointRepository.kt
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/entity/repository/PointRepository.kt
@@ -1,9 +1,7 @@
 package com.xquare.v1servicepoint.point.entity.repository
 
-import com.xquare.v1servicepoint.point.entity.PointEntity
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import com.xquare.v1servicepoint.point.spi.PointSpi
 import org.springframework.stereotype.Repository
-import java.util.UUID
 
 @Repository
-interface PointRepository : CoroutineCrudRepository<PointEntity, UUID>
+class PointRepository : PointSpi

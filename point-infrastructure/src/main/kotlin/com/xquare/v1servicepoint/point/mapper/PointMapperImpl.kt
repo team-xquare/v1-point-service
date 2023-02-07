f751cdd5eb8fd94562443854ddb25bc090ac3dd3
lyutvs <lyutvs@gmail.com>
2023-02-07 14:30:25 +0900

    ⚡️ :: PointMapperImpl

--- point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/mapper/PointMapperImpl.kt
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/mapper/PointMapperImpl.kt
@@ -2,9 +2,10 @@ package com.xquare.v1servicepoint.point.mapper
 
 import com.xquare.v1servicepoint.point.Point
 import com.xquare.v1servicepoint.point.entity.PointEntity
+import org.springframework.stereotype.Component
 
+@Component
 class PointMapperImpl : PointMapper {
-
     override fun pointEntityToDomain(pointEntity: PointEntity): Point {
         return Point(
             id = pointEntity.id,

4089b07d9d0aa715491ef01d2771414de387f957
lyutvs <lyutvs@gmail.com>
2023-02-08 11:28:38 +0900

    ⚡️ :: UseCase작성

--- point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/impl/PointHistoryApiImpl.kt
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/impl/PointHistoryApiImpl.kt
@@ -1,8 +1,10 @@
 package com.xquare.v1servicepoint.point.api.impl
 
 import com.xquare.v1servicepoint.annotation.UseCase
+import com.xquare.v1servicepoint.point.PointHistory
 import com.xquare.v1servicepoint.point.api.PointHistoryApi
 import com.xquare.v1servicepoint.point.api.dto.request.DomainGivePointUserRequest
+import com.xquare.v1servicepoint.point.exception.PointHistoryNotFoundException
 import com.xquare.v1servicepoint.point.exception.PointNotFoundException
 import com.xquare.v1servicepoint.point.exception.UserNotFoundException
 import com.xquare.v1servicepoint.point.spi.PointHistorySpi
@@ -30,5 +32,23 @@ class PointHistoryApiImpl(
         }
 
         pointHistorySpi.saveUserPoint(userId, getPointByPointId.id)
+    }
+
+    override suspend fun deleteUserPoint(studentId: UUID, historyId: UUID) {
+        val pointHistory = pointHistorySpi.findByIdAndStudentId(historyId, studentId)
+            ?: throw PointHistoryNotFoundException(PointHistoryNotFoundException.POINT_HISTORY_NOT_FOUND)
+
+        val getPointByPointId = pointSpi.findByPointId(pointHistory.pointId)
+            ?: throw PointNotFoundException(PointNotFoundException.POINT_NOT_FOUND)
+
+        val pointStatus = pointStatusSpi.findByUserId(pointHistory.userId)
+            ?: throw UserNotFoundException(UserNotFoundException.USER_ID_NOT_FOUND)
+
+        when (getPointByPointId.type) {
+            true -> pointStatus.minusGoodPoint(getPointByPointId.point)
+            false -> pointStatus.minusBadPoint(getPointByPointId.point)
+        }
+
+        pointHistorySpi.deleteByIdAndUserId(pointHistory)
     }
 }

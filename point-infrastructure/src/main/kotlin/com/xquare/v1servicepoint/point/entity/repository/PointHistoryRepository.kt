81bd9bb65726334c91fcd03640f0dda205b8f6f8
lyutvs <lyutvs@gmail.com>
2023-02-08 11:29:39 +0900

    ⚡️ :: delete및 query 부분 작성

--- point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/entity/repository/PointHistoryRepository.kt
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/entity/repository/PointHistoryRepository.kt
@@ -1,7 +1,11 @@
 package com.xquare.v1servicepoint.point.entity.repository
 
 import com.github.f4b6a3.uuid.UuidCreator
+import com.linecorp.kotlinjdsl.ReactiveQueryFactory
+import com.linecorp.kotlinjdsl.deleteQuery
 import com.linecorp.kotlinjdsl.query.HibernateMutinyReactiveQueryFactory
+import com.linecorp.kotlinjdsl.querydsl.expression.col
+import com.linecorp.kotlinjdsl.selectQuery
 import com.xquare.v1servicepoint.point.PointHistory
 import com.xquare.v1servicepoint.point.entity.PointHistoryEntity
 import com.xquare.v1servicepoint.point.mapper.PointHistoryMapper
@@ -35,4 +39,34 @@ class PointHistoryRepository(
 
     private suspend fun Mutiny.Session.persistPointHistoryEntityConcurrently(pointHistoryEntity: PointHistoryEntity) =
         this@persistPointHistoryEntityConcurrently.persist(pointHistoryEntity).awaitSuspending()
+
+    override suspend fun findByIdAndStudentId(id: UUID, studentId: UUID): PointHistory? {
+        return reactiveQueryFactory.transactionWithFactory { _, reactiveQueryFactory ->
+            reactiveQueryFactory.findByIdAndStudentId(id, studentId)
+        }
+    }
+
+    private suspend fun ReactiveQueryFactory.findByIdAndStudentId(id: UUID, studentId: UUID): PointHistory {
+        return this.selectQuery<PointHistory> {
+            where(
+                col(PointHistory::id).equal(id)
+                    .and(col(PointHistory::userId).equal(studentId))
+            )
+        }.singleResult()
+    }
+
+    override suspend fun deleteByIdAndUserId(pointHistory: PointHistory) {
+        reactiveQueryFactory.transactionWithFactory { _, reactiveQueryFactory ->
+            reactiveQueryFactory.deleteWithIdAndUserId(pointHistory.id, pointHistory.userId)
+        }
+    }
+
+    private suspend fun ReactiveQueryFactory.deleteWithIdAndUserId(id: UUID, studentId: UUID) {
+        this.deleteQuery<PointHistoryEntity> {
+            where(
+                col(PointHistoryEntity::id).equal(id)
+                    .and(col(PointHistoryEntity::userId).equal(studentId))
+            )
+        }.executeUpdate()
+    }
 }

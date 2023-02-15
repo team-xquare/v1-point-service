f3d140c2956fcf9cd3dc937b66302cd253a873ed
lyutvs <lyutvs@gmail.com>
2023-02-15 16:27:47 +0900

    ⚡️ :: PointApi

--- point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/PointApi.kt
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/PointApi.kt
@@ -1,9 +1,12 @@
 package com.xquare.v1servicepoint.point.api
 
+import com.xquare.v1servicepoint.point.api.dto.request.DomainUpdatePointRoleRequest
 import com.xquare.v1servicepoint.point.api.dto.response.PointStatusResponse
 import java.util.UUID
 
 interface PointApi {
 
     suspend fun queryPointStatus(userId: UUID): PointStatusResponse
+
+    suspend fun updatePointRole(pointId: UUID, request: DomainUpdatePointRoleRequest)
 }
--- point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/impl/PointApiImpl.kt
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/impl/PointApiImpl.kt
@@ -2,14 +2,18 @@ package com.xquare.v1servicepoint.point.api.impl
 
 import com.xquare.v1servicepoint.annotation.UseCase
 import com.xquare.v1servicepoint.point.api.PointApi
+import com.xquare.v1servicepoint.point.api.dto.request.DomainUpdatePointRoleRequest
 import com.xquare.v1servicepoint.point.api.dto.response.PointStatusResponse
+import com.xquare.v1servicepoint.point.exception.PointNotFoundException
 import com.xquare.v1servicepoint.point.exception.UserNotFoundException
+import com.xquare.v1servicepoint.point.spi.PointSpi
 import com.xquare.v1servicepoint.point.spi.PointStatusSpi
 import java.util.UUID
 
 @UseCase
 class PointApiImpl(
     private val pointStatusSpi: PointStatusSpi,
+    private val pointSpi: PointSpi,
 ) : PointApi {
 
     override suspend fun queryPointStatus(userId: UUID): PointStatusResponse {
@@ -19,6 +23,17 @@ class PointApiImpl(
         return PointStatusResponse(
             goodPoint = getUserPointStatus.goodPoint,
             badPoint = getUserPointStatus.badPoint,
+        )
+    }
+
+    override suspend fun updatePointRole(pointId: UUID, request: DomainUpdatePointRoleRequest) {
+        val point = pointSpi.findByPointId(pointId)
+            ?: throw PointNotFoundException((PointNotFoundException.POINT_NOT_FOUND))
+
+        point.updatePointRole(
+            reason = request.reason,
+            type = request.type,
+            point = request.point,
         )
     }
 }

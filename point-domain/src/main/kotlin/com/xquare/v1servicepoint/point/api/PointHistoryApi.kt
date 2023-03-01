f61109f1a383d5b55ffbd0e26f03ea220bab52f3
lyutvs <lyutvs@gmail.com>
2023-03-01 21:02:57 +0900

    ⚡️ :: queryUserPointHistoryForStudent

--- point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/PointHistoryApi.kt
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/api/PointHistoryApi.kt
@@ -2,6 +2,7 @@ package com.xquare.v1servicepoint.point.api
 
 import com.xquare.v1servicepoint.point.api.dto.request.DomainGivePointUserRequest
 import com.xquare.v1servicepoint.point.api.dto.response.PointHistoryListResponse
+import com.xquare.v1servicepoint.point.api.dto.response.PointHistoryListStudentResponse
 import java.util.UUID
 
 interface PointHistoryApi {
@@ -10,6 +11,8 @@ interface PointHistoryApi {
     suspend fun deleteUserPoint(studentId: UUID, historyId: UUID)
 
     suspend fun queryUserPointHistory(userId: UUID, type: Boolean): PointHistoryListResponse
+
+    suspend fun queryUserPointHistoryForStudent(userId: UUID, type: Boolean): PointHistoryListStudentResponse
 
     suspend fun savePointStatus(userId: UUID)
 }

1a61073fc66fc75740c9e9c437f5d52c357bc94f
lyutvs <lyutvs@gmail.com>
2023-03-05 02:22:52 +0900

    ⚡️ :: PointHistoryRepositorySpi

--- point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/spi/PointHistoryRepositorySpi.kt
+++ point-domain/src/main/kotlin/com/xquare/v1servicepoint/point/spi/PointHistoryRepositorySpi.kt
@@ -3,6 +3,7 @@ package com.xquare.v1servicepoint.point.spi
 import com.xquare.v1servicepoint.annotation.Spi
 import com.xquare.v1servicepoint.point.PointHistory
 import com.xquare.v1servicepoint.point.api.dto.response.PointHistoryElement
+import com.xquare.v1servicepoint.point.api.dto.response.PointHistoryExcelElement
 import java.util.UUID
 
 @Spi
@@ -16,4 +17,8 @@ interface PointHistoryRepositorySpi {
     suspend fun findAllByUserIdAndType(userId: UUID, type: Boolean?): List<PointHistoryElement>
 
     suspend fun findAllByPointId(pointId: UUID): List<PointHistory>
+
+    suspend fun findByUserId(userId: UUID): PointHistory?
+
+    suspend fun findAllByIdAndType(idList: List<UUID>, type: Boolean?): List<PointHistoryExcelElement>
 }

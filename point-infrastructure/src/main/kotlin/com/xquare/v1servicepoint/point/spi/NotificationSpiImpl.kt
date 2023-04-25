c243238de92b7b6bd57a1c806103aaeb1087a45a
lyutvs <lyutvs@gmail.com>
2023-04-25 11:20:07 +0900

    📑 :: NotificationSpiImpl

---
+++ point-infrastructure/src/main/kotlin/com/xquare/v1servicepoint/point/spi/NotificationSpiImpl.kt
@@ -0,0 +1,42 @@
+package com.xquare.v1servicepoint.point.spi
+
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.model.MessageAttributeValue
+import com.amazonaws.services.sqs.model.SendMessageRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.xquare.v1servicepoint.point.api.dto.request.DomainSendMessageRequest
+import org.springframework.stereotype.Component
+import java.util.*
+
+
+@Component
+class NotificationSpiImpl(
+    private val objectMapper: ObjectMapper,
+    private val amazonSQS: AmazonSQS,
+) : NotificationSpi {
+
+    companion object {
+        const val NOTIFICATION_FIFO = "notification.fifo"
+    }
+
+    override suspend fun sendNotification(userId: UUID, topic: String, content: String, threadId: String) {
+        val domainSendMessageRequest = DomainSendMessageRequest(
+            userId, topic, content, threadId
+        )
+
+        val sendMessageRequest = SendMessageRequest(
+            amazonSQS.getQueueUrl(NOTIFICATION_FIFO).queueUrl,
+            objectMapper.writeValueAsString(domainSendMessageRequest),
+        )
+            .withMessageGroupId("point")
+            .withMessageDeduplicationId(UUID.randomUUID().toString())
+            .withMessageAttributes(
+                mapOf(
+                    "Content-Type" to MessageAttributeValue()
+                        .withDataType("String")
+                        .withStringValue("application/json")
+                )
+            )
+        amazonSQS.sendMessage(sendMessageRequest)
+    }
+}
